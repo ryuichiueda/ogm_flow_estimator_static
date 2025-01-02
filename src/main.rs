45c6761fe78261d1c5273ca2b7f84182550c5124
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-01-02 10:01:16 +0900

    Purge a publisher

--- src/main.rs
+++ src/main.rs
@@ -11,40 +11,36 @@ use rclrs::Clock;
 use builtin_interfaces::msg::Time;
 use geometry_msgs::msg::Pose;
 
-struct RepublisherNode {
+struct FlowEstimatorNode {
     node: Arc<rclrs::Node>,
-    _subscription: Arc<rclrs::Subscription<LaserScan>>,
+    _sub_scan: Arc<rclrs::Subscription<LaserScan>>,
     data: Arc<Mutex<Option<LaserScan>>>,
-    publisher: Arc<rclrs::Publisher<LaserScan>>,
     obstacle_map: Arc<rclrs::Publisher<OccupancyGrid>>,
 }
 
-impl RepublisherNode {
+impl FlowEstimatorNode {
     fn new(context: &rclrs::Context) -> Result<Self, rclrs::RclrsError> {
         let node = rclrs::Node::new(context, "republisher")?;
         let data = Arc::new(Mutex::new(None));
         let data_cb = Arc::clone(&data);
-        let _subscription = node.create_subscription(
+        let _sub_scan = node.create_subscription(
             "scan",
             rclrs::QOS_PROFILE_DEFAULT,
             move |msg: LaserScan| { *data_cb.lock().unwrap() = Some(msg); },
         )?;
 
-        let publisher = node.create_publisher("out_topic", rclrs::QOS_PROFILE_DEFAULT)?;
         let obstacle_map = node.create_publisher("obstacle_map", rclrs::QOS_PROFILE_DEFAULT)?;
         Ok(Self {
             node,
-            _subscription,
-            publisher,
+            _sub_scan,
             data,
             obstacle_map,
         })
     }
 
     fn republish(&self) -> Result<(), rclrs::RclrsError> {
-        if let Some(s) = &*self.data.lock().unwrap() {
-            self.publisher.publish(s)?;
-        }
+        let scan = self.data.lock().unwrap();
+        dbg!("{:?}", &scan);
 
         let clock = Clock::system();
         let now = clock.now();
@@ -76,7 +72,7 @@ impl RepublisherNode {
                          0+117, 3+117, 5+117, 7+117, 10+117, 12+117,
                          0+130, 5+130, 7+130, 12+130,
                          0+143, 1+143, 2+143, 5+143, 7+143, 8+143, 9+143, 12+143,
-                         4+156, 5+156, 11+156, 12+156
+                         4+156, 5+156, 11+156
         ];
         for b in black {
             tmp[b] = 0;
@@ -97,7 +93,7 @@ impl RepublisherNode {
 
 fn main() -> Result<(), rclrs::RclrsError> {
     let context = rclrs::Context::new(std::env::args())?;
-    let republisher = Arc::new(RepublisherNode::new(&context)?);
+    let republisher = Arc::new(FlowEstimatorNode::new(&context)?);
     let republisher_other_thread = Arc::clone(&republisher);
 
     std::thread::spawn(move || -> Result<(), rclrs::RclrsError> {
