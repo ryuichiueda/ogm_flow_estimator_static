82f25097db18d2cc934d01f1f031f0a15f1c9980
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-02-11 11:46:40 +0900

    Add source

--- test/test.bash
+++ test/test.bash
@@ -3,5 +3,9 @@
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
+#cd $dir/ros2_ws
+#colcon build
+source $dir/.bashrc
+
 cd $dir/ros2_ws/src/ogm_flow_estimator_static
 cargo build --release
