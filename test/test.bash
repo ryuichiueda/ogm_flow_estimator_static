3a8d7210fa1fc319681d34bdcb025c1825624475
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-02-11 11:49:06 +0900

    Add another build

--- test/test.bash
+++ test/test.bash
@@ -3,8 +3,8 @@
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
-#cd $dir/ros2_ws
-#colcon build
+cd $dir/ros2_ws
+colcon build
 source $dir/.bashrc
 
 cd $dir/ros2_ws/src/ogm_flow_estimator_static
