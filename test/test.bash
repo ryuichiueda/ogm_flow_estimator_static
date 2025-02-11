fe70f0fc72ba84608357f593876a13821f98a1ca
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-02-11 12:56:58 +0900

    Simplify

--- test/test.bash
+++ test/test.bash
@@ -3,14 +3,14 @@
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
-source $dir/.bashrc
-cd $dir/ros2_ws
-colcon build
+#source $dir/.bashrc
+#cd $dir/ros2_ws
+#colcon build
 source $dir/.bashrc
 
 cd $dir/ros2_ws/src/ogm_flow_estimator_static
 cargo build --release
 
 ( sleep 5 && ros2 bag play ./bag/rosbag2_2025_01_22-13_29_26 ) &
-timeout cargo run --release |& tee - log.txt
+timeout 30 cargo run --release |& tee - log.txt
 
