dc701906671f77902a75fadacc2e59fda10c6a7c
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-02-11 12:59:22 +0900

    Rewind

--- test/test.bash
+++ test/test.bash
@@ -3,9 +3,9 @@
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
-#source $dir/.bashrc
-#cd $dir/ros2_ws
-#colcon build
+source $dir/.bashrc
+cd $dir/ros2_ws
+colcon build
 source $dir/.bashrc
 
 cd $dir/ros2_ws/src/ogm_flow_estimator_static
